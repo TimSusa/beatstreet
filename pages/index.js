4c7e348655d5ff6f83d84b30f26b32730bfbb625
TIM <minitim@minizoo.fritz.box>
2022-03-01 20:13:16 +0100

    feat: Chat

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import dynamic from "next/dynamic";
+const AblyChatComponent = dynamic(
+  () => import("../components/AblyChatComponent"),
+  { ssr: false }
+);
 
 export default function Home() {
   return (
@@ -13,58 +18,28 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         <a href="https://beatstreet.dance">beatstreet!</a>
+          <a href="https://beatstreet.dance">beatstreet</a>
         </h1>
 
-        <p className={styles.description}>
-          Underground Dance Music Nonstop!{' '}
-        </p>
+        <p className={styles.description}>Underground Dance Music Nonstop </p>
 
-        <div className={styles.grid}>
-           <a href="https://fm.soundzmuzicradio.com/public/soundzmuzicradio" className={styles.card}>
-            <p><iframe 
-            src="https://fm.soundzmuzicradio.com/public/soundzmuzicradio" 
-            border="none" allow-transparency="true" title="beatstreet" 
-            height="150px" width="400px" style={{borderRadius: 5, overflow: 'hidden'}}></iframe></p>
-          </a> 
-
-          {/* <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a> */}
+        <iframe
+          className={styles.card}
+          src="https://fm.soundzmuzicradio.com/public/soundzmuzicradio/embed?theme=dark"
+          frameBorder="1"
+          style={{ minHeight: 100, minWidth: "70%", border: 0 }}
+        ></iframe>
+        <iframe
+          src="https://fm.soundzmuzicradio.com/public/soundzmuzicradio/history?theme=dark"
+          frameBorder="0"
+          style={{ minHeight: 150, minWidth: "70%" }}
+        ></iframe>
+        <div className={styles.card}>
+          <AblyChatComponent />{" "}
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
