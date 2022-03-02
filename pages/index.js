e106eb911c0e7f9990611727434bf0f72da79076
TIM <minitim@minizoo.fritz.box>
2022-03-02 06:42:29 +0100

    feat: discord

--- pages/index.js
+++ pages/index.js
@@ -49,18 +49,16 @@ export default function Home() {
           className={styles.card}
           style={{ minHeight: 150, minWidth: "90%" }}
         ></iframe>
-        <iframe
+        {/* <iframe
           className={styles.card}
           src="https://discord.com/widget?id=948422756477833216&theme=dark"
           width="650"
           height="600"
-          allowTransparency={true}
           frameBorder="0"
           id= "chatwidget"
           name= "chatwidget"
-          sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-        ></iframe>
-
+          style={{ minHeight: 150, minWidth: "90%" }}
+        ></iframe> */}
         <iframe
           className={styles.card}
           src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23039BE5&ctz=America%2FNew_York&mode=WEEK&showPrint=0&showTitle=0&showNav=0&showTabs=1&showCalendars=1&src=YmVhdHN0cmVldHBvc3RAZ21haWwuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uZ2VybWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043"
@@ -68,6 +66,10 @@ export default function Home() {
           frameBorder="0"
           scrolling="no"
           style={{ minWidth: "90%" }}
+        ></iframe>
+        <iframe src="https://e.widgetbot.io/channels/948422756477833216/948435054995050556" height="600" 
+        width="800"
+        style={{ minHeight: 150, minWidth: "90%" }}
         ></iframe>
         <div className={styles.card}>
           <AblyChatComponent />{" "}
